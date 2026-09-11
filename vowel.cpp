93f88235636dca57ae0596282766a9034b474f43
Sushant Kumar <sushantkumar9707@gmail.com>
2026-09-11 10:23:41 +0530

    git commit -m "Added string palindrome and vowel checking programs"

---
+++ vowel.cpp
@@ -0,0 +1,21 @@
+//count number of vowels in a string
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    int count = 0;
+    for(int i = 0; i < str.length(); i++){
+        char ch = (str[i]);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            count++;
+        }
+    }
+    cout << "Number of vowels in the string: " << count << endl;
+
+    return 0;
+}
