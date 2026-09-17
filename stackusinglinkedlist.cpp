a40017785cbd3b038bf705a05bcf505c2f1d60f5
Sushant Kumar <sushantkumar9707@gmail.com>
2026-09-17 15:58:12 +0530

    git commit -m "Added stack implementation using linked list"

---
+++ stackusinglinkedlist.cpp
@@ -0,0 +1,59 @@
+//stack using linked list
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+
+class Stack{
+    private:
+    Node* top;
+    public:
+    Stack(){
+        top = nullptr;
+    }
+    void push(int value){
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+    void pop(){
+        if(top == nullptr){
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+    int peek(){
+        if(top == nullptr){
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->data;
+    }
+    bool isEmpty(){
+        return top == nullptr;
+    }
+};
+
+int main(){
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << "Top element is: " << s.peek() << endl;
+    s.pop();
+    cout << "Top element after pop is: " << s.peek() << endl;
+    return 0;
+}
+
